737b069d8248ba3721a1e240c6da222147ffeb83
zhouchch3 <2835079913@qq.com>
2022-03-21 13:42:22 +0000

    feat(cls_plt_fig): 1. uniform font_size=14; 2. shift right second y;

--- scripts/cls_plt_fig.py
+++ scripts/cls_plt_fig.py
@@ -25,14 +25,14 @@ class cls_plt_fig():
 
             # plot:
             y_linestyle='-',
-            y_marker=[['s', 's'], ['o', 'o'], ['*', '*']],
+            y_marker=[['s', 's'], ['*', '*'], ['o', 'o']],
             y_markersize=10,
             # y_markerfacecolor=[[None]],
             y_markeredgecolor="white",
             y_markeredgewidth=1,
             # bar:
             y_edgecolor = [[None]],
-            # y1_facecolor = None
+            y_facecolor = [[None]],
             y_hatch = [[None]],
 
             # global setting
@@ -40,35 +40,35 @@ class cls_plt_fig():
             # legend_size= 12,
             # label_size= 12,
             # xticks_fontsize= 12,
-            font_size = 12,
+            font_size = 14,
             linewidth= 2,
-            bar_width = 0.1,
+            bar_width = 0.4,
             bar_linewidth = 0.6,
 
             grid_axis= None,
             plt_text= False,
             legend_loc= "best",
             legend_ncol=1,
-            figsize = (7, 4.6)
+            figsize = (7, 4)
             ): # dict
         y_dim = np.shape(y_value)
 
-        
-        
         if y_dim[0] >2: # multi Y axis using HostAxes method but can't change fontsize
             fig = plt.figure() #定义figure，（1）中的1是什么
-            ax = HostAxes(fig, [0, 0, 0.6, 0.6])  #用[left, bottom, weight, height]的方式定义axes，0 <= l,b,w,h <= 1
+            ax = HostAxes(fig, [0.2, 0.2, 0.6, 0.6])  #用[left, bottom, weight, height]的方式定义axes，0 <= l,b,w,h <= 1
         else:
             fig, ax = plt.subplots(figsize=figsize)
-
+        plt.rcParams['hatch.color'] = "white"
         bar_gap_width = bar_width/4
         ax1 = None
         ax_array = [ax]
         if y_dim[0] > 1:
-            ax.axis['right'].set_visible(False)
-            ax.axis['top'].set_visible(False)
+
 
             if y_dim[0] >2:
+                ax.axis['right'].set_visible(False)
+                ax.axis['top'].set_visible(False)
+
                 ax1 = ParasiteAxes(ax, sharex=ax)
                 ax.parasites.append(ax1)
                 ax1.axis['top'].set_visible(False)
@@ -93,15 +93,16 @@ class cls_plt_fig():
         for idx_axis in range(y_dim[0]):
             for idx_vector in range(y_dim[1]):
                 if  y_fig_type[idx_axis][idx_vector] == "plot":
+                    print(idx_axis, idx_vector, x_value + 0.6*idx_vector)
                     ax_array[idx_axis].plot(
-                            x_value + 0.6*idx_vector, 
+                            (x_value + 0.6*idx_vector)*(1+ 0.02/1.8*idx_axis),
                             y_value[idx_axis][idx_vector], 
                             label=y_legend[idx_axis][idx_vector],
                             # linewidth=linewidth, 
                             linestyle=y_linestyle, 
                             color=y_color[idx_axis][idx_vector],  
                             marker=y_marker[idx_axis][idx_vector], 
-                            markersize=y_markersize if y_marker[idx_axis][idx_vector] !="*" else y_markersize+8, 
+                            markersize=y_markersize if y_marker[idx_axis][idx_vector] !="*" else y_markersize+10, 
                             markeredgecolor = y_markeredgecolor, 
                             markeredgewidth = y_markeredgewidth,
                             zorder=10
@@ -121,8 +122,9 @@ class cls_plt_fig():
                             label=y_legend[idx_axis][idx_vector],
                             linewidth=bar_linewidth,
                             width=bar_width, 
-                            facecolor= y_color[idx_axis][idx_vector],
-                            edgecolor= y_edgecolor[idx_axis][idx_vector], 
+                            color= y_color[idx_axis][idx_vector],
+                            # facecolor= y_facecolor[idx_axis][idx_vector],
+                            # edgecolor= y_edgecolor[idx_axis][idx_vector], 
                             # hatch = y_hatch[idx_axis][idx_vector], 
                             zorder=10)
 
@@ -136,7 +138,7 @@ class cls_plt_fig():
             )
 
             # yticks
-            ax_array[idx_axis].tick_params(axis='y', which='major',width=1.5, length=8, labelsize = font_size+2, colors=y_axis_color[idx_axis], direction='out')
+            ax_array[idx_axis].tick_params(axis='y', which='major',width=1.5, length=6, labelsize = font_size, colors=y_axis_color[idx_axis], direction='out')
             if y_yticks_min and y_yticks_max[idx_axis] and len_yticks:
                 ax_array[idx_axis].set_yticks(np.linspace(y_yticks_min[idx_axis], y_yticks_max[idx_axis], len_yticks+1)) # occupy fully yaxis
                 ax_array[idx_axis].set_ylim(y_yticks_min[idx_axis],y_yticks_max[idx_axis])
@@ -158,9 +160,9 @@ class cls_plt_fig():
         # x ticks
         # ax.set_xlim(x_value[0], x_value[-1])    
         # ax.set_xlim(x_value[0]-bar_width*2, x_value[-1]+bar_width*2)
-        plt.xticks(xticks,xticks if xticks is not None else x_value)# ["0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2"])
+        plt.xticks(xticks, ["0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2"])
             # xticks if xticks is not None else x_value)
-        ax.tick_params(axis='x', which='major',width=1.5, length=8, labelsize = font_size+2, colors=(0,0,0), direction='out')
+        ax.tick_params(axis='x', which='major',width=1.5, length=6, labelsize = font_size, colors=(0,0,0), direction='out')
 
         plt.grid(axis=grid_axis, zorder=0) #,
 
@@ -179,8 +181,9 @@ class cls_plt_fig():
             ax_array[1].spines['right'].set_color(y_axis_color[1])
 
 
-        # handles1 = [handles1[0]] + [handles1[2]] + [handles1[4]]
-        # labels1 = [labels1[0]] + [labels1[2]] + [labels1[4]]
-        plt.legend(handles1, labels1, fontsize=font_size, loc=legend_loc, ncol=legend_ncol, frameon=False)#, handletextpad=0.1,columnspacing=0.4 )
+        handles1 = [handles1[0]] + [handles1[2]]
+        labels1 = [labels1[0]] + [labels1[2]]
+        plt.legend(handles1, labels1, fontsize=font_size, loc=legend_loc, ncol=legend_ncol, frameon=False) #, handletextpad=0.1,columnspacing=0.4 )
+        plt.rcParams['hatch.color'] = "white"
         plt.savefig(fig_name, format='svg')
         plt.show()
